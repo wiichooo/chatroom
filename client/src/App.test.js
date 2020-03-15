d57fdbe8d4c19b6802611cb91b4f9fc6b0d3c5c6
Luis Godinez <wiichooo@gmail.com>
2020-03-15 12:01:41 -0600

    test: adding server side tests, socket.io, new message and new client tests.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,41 +1,156 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-import Login from './views/login'
-import Chat from './views/chat'
+import React from "react";
+import { render } from "@testing-library/react";
+import Login from "./views/login";
+import Chat from "./views/chat";
+import io from "socket.io-client";
 
-test('renders login', () => {
-  const { getByText } = render(<Login />);
-  const linkElement = getByText(/Join!/);
-  expect(linkElement).toBeInTheDocument();
+//Client Side Tests
+describe("CLIENT SIDE test suit: Login and Chat Components", () => {
+  test("renders login", () => {
+    const { getByText } = render(<Login />);
+    const linkElement = getByText(/Join!/);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("renders login", () => {
+    const { getByText } = render(<Login />);
+    const linkElement = getByText(/username/);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("renders chat", () => {
+    const { getByText } = render(
+      <Chat users={["a", "b"]} msgs={[{ content: "a", username: "b" }]} />
+    );
+    const linkElement = getByText(/Connected Users:/);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("renders chat", () => {
+    const { getByText } = render(
+      <Chat users={["a", "b"]} msgs={[{ content: "a", username: "b" }]} />
+    );
+    const linkElement = getByText(/stooq.com/);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("renders chat", () => {
+    const { getByText } = render(
+      <Chat users={["a", "b"]} msgs={[{ content: "a", username: "b" }]} />
+    );
+    const linkElement = getByText(/Messages/);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("renders chat", () => {
+    const { getByText } = render(
+      <Chat users={["a", "b"]} msgs={[{ content: "a", username: "b" }]} />
+    );
+    const linkElement = getByText(/Last line/);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
 
-test('renders login', () => {
-  const { getByText } = render(<Login />);
-  const linkElement = getByText(/username/);
-  expect(linkElement).toBeInTheDocument();
-});
+///Server Side Tests
+const initSocket = () => {
+  return new Promise((resolve, reject) => {
+    // create socket for communication
+    const socket = io("http://127.0.0.1:4000");
 
-test('renders chat', () => {
-  const { getByText } = render(<Chat users={['a','b']} msgs={[{content:'a', username:'b'}]}/>);
-  const linkElement = getByText(/Connected Users:/);
-  expect(linkElement).toBeInTheDocument();
-});
+    // define event handler for sucessfull connection
+    socket.on("connect", () => {
+      console.log("connected");
+      resolve(socket);
+    });
 
-test('renders chat', () => {
-  const { getByText } = render(<Chat users={['a','b']} msgs={[{content:'a', username:'b'}]}/>);
-  const linkElement = getByText(/stooq.com/);
-  expect(linkElement).toBeInTheDocument();
-});
+    // if connection takes longer than 5 seconds throw error
+    setTimeout(() => {
+      reject(new Error("Failed to connect wihtin 5 seconds."));
+    }, 5000);
+  });
+};
 
-test('renders chat', () => {
-  const { getByText } = render(<Chat users={['a','b']} msgs={[{content:'a', username:'b'}]}/>);
-  const linkElement = getByText(/Messages/);
-  expect(linkElement).toBeInTheDocument();
-});
+const destroySocket = socket => {
+  return new Promise((resolve, reject) => {
+    // check if socket connected
+    if (socket.connected) {
+      // disconnect socket
+      console.info("disconnecting...");
+      socket.disconnect();
+      resolve(true);
+    } else {
+      // not connected
+      console.log("no connection to break...");
+      resolve(false);
+    }
+  });
+};
 
-test('renders chat', () => {
-  const { getByText } = render(<Chat users={['a','b']} msgs={[{content:'a', username:'b'}]}/>);
-  const linkElement = getByText(/Last line/);
-  expect(linkElement).toBeInTheDocument();
+describe("SERVER SIDE test suit: NewUsername and NewMesaage", () => {
+  test("test: NewUsername", async () => {
+    // create socket for communication
+    const socketClient = await initSocket();
+    // create new promise for server response
+    const serverResponse = new Promise((resolve, reject) => {
+      // define a handler for the test event
+      socketClient.on("update current users", clientData => {
+        //process data received from server
+        const { username } = clientData;
+        console.log("Server says: " + username);
+        destroySocket(socketClient);
+        resolve(clientData);
+      });
+      setTimeout(() => {
+        reject(new Error("Failed to get reponse, connection timed out..."));
+      }, 5000);
+    });
+
+    const serverData = {
+      status: 200,
+      username: "Fernando"
+    };
+
+    // emit event with data to server
+    console.log("Emitting NEW CLIENT event");
+    socketClient.emit("new client username", serverData);
+    const res  = await serverResponse;
+    const usernames = res.shift();
+    // check the response data
+    expect(usernames.status).toBe(200);
+    expect(usernames.username).toBe("Fernando");
+  });
+
+  test("test: NewMessage", async () => {
+    // create socket for communication
+    const socketClient = await initSocket();
+    // create new promise for server response
+    const serverResponse = new Promise((resolve, reject) => {
+      // define a handler for the test event
+      socketClient.on("new message", clientData => {
+        //process data received from server
+        const { content } = clientData;
+        console.log("Server says: " + content);
+        destroySocket(socketClient);
+        resolve(clientData);
+      });
+      setTimeout(() => {
+        reject(new Error("Failed to get reponse, connection timed out..."));
+      }, 5000);
+    });
+
+    const serverData = {
+      content: "MSG CONTENT",
+      status: 200,
+      username: "Luis"
+    };
+
+    // emit event with data to server
+    console.log("Emitting NEW MSG event");
+    socketClient.emit("send message", serverData);
+    const { status, content, username } = await serverResponse;
+    // check the response data
+    expect(status).toBe(200);
+    expect(username).toBe("Luis");
+    expect(content).toBe("MSG CONTENT");
+  });
 });
